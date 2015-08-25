c2c5fa74283d3a7f233678a70258e20323a2e22f
wensh <wensh@itdayang.com>
2015-08-25 14:14:22 +0800

    Initial Commit

---
+++ iwf/iwf.h
@@ -0,0 +1,19 @@
+//
+//  iwf.h
+//  iwf
+//
+//  Created by Centny on 8/25/15.
+//  Copyright (c) 2015 Snows. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iwf.
+FOUNDATION_EXPORT double iwfVersionNumber;
+
+//! Project version string for iwf.
+FOUNDATION_EXPORT const unsigned char iwfVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iwf/PublicHeader.h>
+
+
